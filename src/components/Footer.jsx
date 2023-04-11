ba9cc2eb0432cdf1aea3335e331f71d267feb42f
Igor <igoraraujobda@gmail.com>
2023-04-11 11:41:12 -0300

    ajuste texto footer e cor do icone de login

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@
 export default function Footer() {
   return (
     <footer>
-      <p>My Costumer -  All rights not reserved - 2023</p>
+      <p>Desenvolvido por <a href='https://github.com/igor93araujo'>Igor Barbosa</a> - All rights not reserved - 2023</p>
     </footer>
   )
 }
